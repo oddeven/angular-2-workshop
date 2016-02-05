6e805075fc584eb2fd49d9075f7c2eb1035570e1
Angular 2 Workshop <workshop-guest@oddeven.ch>
2016-02-05 19:24:10 +0100

    Added NgFor with local view variables, input with reference that is used in a add button and simple pipe

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
 import {Component} from '@angular/core';
+import {ToUpperPipe} from './to-upper-pipe';
 
 @Component({
   selector: 'app',
   template: `
-    <p>{{message}}</p>
-  `
+    <p *ngFor="let message of messages; let i = index">{{i}}: {{message | toUpper}}</p>
+    <input #inp type="text">
+    <button (click)="addMessage(inp.value)">Add</button>
+  `,
+  pipes: [ToUpperPipe]
 })
 export class App {
-  message: String = 'Hello World!';
+  messages: Array<String> = ['One', 'Two', 'Three'];
+
+  addMessage(message: String) {
+    this.messages.push(message);
+  }
 }
