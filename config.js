33878bae98687a788d9b4fe05250cbe0db1c3272
Gion Kunz <gion.kunz@gmail.com>
2016-06-15 10:18:00 +0200

    Updated resource loading for RC1 and removed JSPM

--- config.js
+++ config.js
@@ -1,19 +1,44 @@
-System.config({
-  baseURL: "/",
-  defaultJSExtensions: true,
-  transpiler: "typescript",
-  paths: {
-    "npm:*": "jspm_packages/npm/*"
-  },
+(function(global) {
 
-  packages: {
-    src: {
-      defaultJSExtensions: false,
-      defaultExtension: 'ts'
-    }
-  },
+  var version = '@2.0.0-rc.1';
+  var map = {
+    'app': 'src',
+    'rxjs': 'https://npmcdn.com/rxjs@5.0.0-beta.6'
+  };
 
-  map: {
-    "typescript": "npm:typescript@1.8.0"
-  }
-});
+  var packages = {
+    'src': { main: 'main.ts',  defaultExtension: 'ts' },
+    'rxjs': { defaultExtension: 'js' },
+    'angular2-in-memory-web-api': { defaultExtension: 'js' }
+  };
+
+  var packageNames = [
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/core',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    '@angular/testing'
+  ];
+
+  packageNames.forEach(function(pkgName) {
+    map[pkgName] = 'https://npmcdn.com/' + pkgName + version;
+  });
+
+  packageNames.forEach(function(pkgName) {
+    packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  });
+
+  var config = {
+    transpiler: 'typescript',
+    typescriptOptions: {
+      emitDecoratorMetadata: true
+    },
+    map: map,
+    packages: packages
+  };
+  System.config(config);
+
+})(this);
