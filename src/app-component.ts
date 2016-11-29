4306df24c7a1657cb5c45baa8bf3efae72505619
Gion Kunz <gion.kunz@oddeven.ch>
2016-11-29 21:48:45 +0100

    Update 2.2.3

--- src/app-component.ts
+++ src/app-component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {Collapsible} from './collapsible';
 
 @Component({
   selector: 'app',
@@ -8,9 +7,8 @@ import {Collapsible} from './collapsible';
       <span collapsible-title>I'm a title</span>
       <p>I'm regular content that will be inserted into the collapsible body</p>
     </collapsible>
-  `,
-  directives: [Collapsible]
+  `
 })
-export class App {
+export class AppComponent {
 
 }
